1e8cc1b340b5cfd1ae3fbe10b40d475b118324b2
dicdesign <168957319+dicdesign@users.noreply.github.com>
2024-06-16 14:54:00 +0530

    Update synthesize.sh

--- synthesize.sh
+++ synthesize.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Set your design and top module name
-DESIGN="counter"
-TOP_MODULE_NAME="counter_top_module_name"
+DESIGN="if single file use just that file name else if multiple verilog files then use *"
+TOP_DESIGN="Name of the top module"
+TOP_MODULE_NAME="Name of the top module"
 
 # Use the variables in the script
 yosys -p "
@@ -12,13 +13,13 @@ synth -top ${TOP_MODULE_NAME}
 dfflibmap -liberty sky130_fd_sc_hd__tt_025C_1v80.lib
 abc -liberty sky130_fd_sc_hd__tt_025C_1v80.lib
 clean
-write_verilog -noattr ${DESIGN}.netlist.v
-tee -o ${DESIGN}.rpt stat -liberty sky130_fd_sc_hd__tt_025C_1v80.lib
-write_json ${DESIGN}.json
+write_verilog -noattr ${TOP_DESIGN}.netlist.v
+tee -o ${TOP_DESIGN}.rpt stat -liberty sky130_fd_sc_hd__tt_025C_1v80.lib
+write_json ${TOP_DESIGN}.json
 "
 
 # Generate SVG using netlistsvg
-!netlistsvg ${DESIGN}.json -o ${DESIGN}.svg
+!netlistsvg ${TOP_DESIGN}.json -o ${TOP_DESIGN}.svg
 
 # Open SVG with Inkscape
-!inkscape ${DESIGN}.svg
+!inkscape ${TOP_DESIGN}.svg
